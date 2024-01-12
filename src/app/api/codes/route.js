e42a3233aa6fb9ccbb6d654b663aab1a0df9d38b
Manish Singh <manishsingh13275@gmail.com>
2024-01-12 19:48:04 +0530

    redeployment

--- src/app/api/codes/route.js
+++ src/app/api/codes/route.js
@@ -2,38 +2,38 @@ import { connectToDatabase } from "../../../lib/mongodb";
 import { NextRequest, NextResponse } from "next/server";
 
 function generateCode() {
-    const length = 6;
-    const charset = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    let code = '';
+  const length = 6;
+  const charset =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let code = "";
 
-    for (let i = 0; i < length; i++) {
-        const randomIndex = Math.floor(Math.random() * charset.length);
-        code += charset[randomIndex];
-    }
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * charset.length);
+    code += charset[randomIndex];
+  }
 
-    return code;
+  return code;
 }
 
 export async function GET(request) {
+  try {
+    const currentTimeUTC = new Date().getTime();
+    const { client, db } = await connectToDatabase();
 
-    try {
-        const currentTimeUTC = new Date().getTime();
-        const { client, db } = await connectToDatabase();
+    console.log(currentTimeUTC);
 
-        console.log(currentTimeUTC);
+    const code = generateCode();
 
-        const code = generateCode();
+    await db.collection("codes").insertOne({
+      code: code,
+      generatedAt: currentTimeUTC,
+    });
 
-        await db.collection('codes').insertOne({
-            code: code,
-            generatedAt: currentTimeUTC,
-        });
-
-        return NextResponse.json({
-            code: code,
-            starttime: currentTimeUTC,
-        });
-    } catch (error) {
-        NextResponse.error(error);
-    }
+    return NextResponse.json({
+      code: code,
+      starttime: currentTimeUTC,
+    });
+  } catch (error) {
+    NextResponse.error("Error", error);
+  }
 }
